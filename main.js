936c5f833cfddf8634665be8b89601f2561e0870
bernsamp <119073813+bernsamp@users.noreply.github.com>
2022-12-28 22:18:38 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+window.onload = function() {
+
+    let btn = document.getElementById("jump");
+
+    let count = 0;
+
+
+
+    btn.onclick = function() {
+
+        count += 1;
+    }
+
+    var canvas = document.getElementById("canvas");
+    var context = canvas.getContext("2d");
+    var x = 300;
+    var y = 350;
+
+    context.arc(x, y, 50, 0, 2 * Math.PI);
+    context.fillStyle="red";
+    context.fill();   
+} 
